a71476b3fe3ce0c0aa53df8c9c132bee3ce1ba8c
fsindustry <fuzhengxin@kingsoft.com>
2023-06-26 16:51:32 +0800

    committed by fsindustry @20230626 about: 1) 基于select的echo client编码完成；

--- network/echo/d3_select_echo_client.cpp
+++ network/echo/d3_select_echo_client.cpp
@@ -51,16 +51,50 @@ int main(int argc, char **argv) {
 }
 
 void str_cli(FILE *fp, int sockfd) {
+
+  int maxfd, n, stdineof = 0;
+  fd_set rset;
+  FD_ZERO(&rset);
+
   char write_buf[BUFFER_LENGTH], read_buf[BUFFER_LENGTH];
-  // 获取控制台输入
-  while (Fgets(write_buf, BUFFER_LENGTH, fp)) {
-    // 向服务端发送输入
-    Writen(sockfd, write_buf, strlen(write_buf));
-    // 读取服务端响应
-    if (Readline(sockfd, read_buf, BUFFER_LENGTH) == 0) {
-      err_quit("str_cli: server terminated prematurely");
+
+  for (;;) {
+
+    // 仅当stdin未关闭，才注册stdin读事件监听
+    if (stdineof == 0) {
+      FD_SET(fileno(fp), &rset);
     }
-    // 输出响应到客户端
-    Fputs(read_buf, stdout);
+    // 注册sockfd的读事件监听
+    FD_SET(sockfd, &rset);
+
+    maxfd = max(fileno(fp), sockfd) + 1;
+    select(maxfd, &rset, NULL, NULL, NULL);
+
+    if (FD_ISSET(sockfd, &rset)) { // sockfd可读
+      // 读取服务端响应
+      if ((n = read(sockfd, read_buf, BUFFER_LENGTH)) <= 0) {
+        if (stdineof == 1) {
+          return;
+        } else {
+          err_quit("str_cli: server terminated prematurely");
+        }
+      }
+
+      // 输出响应到客户端
+      write(fileno(stdout), read_buf, n);
+    }
+
+    if (FD_ISSET(fileno(fp), &rset)) { // stdin可读
+      // 获取控制台输入，若未读取到数据，则关闭socket写端
+      if((n = read(fileno(fp), write_buf, BUFFER_LENGTH)) <= 0){
+        stdineof = 1;
+        shutdown(sockfd, SHUT_WR);
+        FD_CLR(fileno(fp), &rset);
+        continue;
+      }
+
+      // 向服务端发送输入
+      Writen(sockfd, write_buf, n);
+    }
   }
 }
